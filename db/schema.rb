0efb5e3fbb4cfd6286ba3c38a8b7bab52d3c93a5
abcussi <cussi.angel.benjamin@gmail.com>
2020-04-27 09:10:00 -0400

    add post model and migrate db add validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_120418) do
+ActiveRecord::Schema.define(version: 2020_04_27_130224) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.string "img_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2020_04_27_120418) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
