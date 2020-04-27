9b6181794529f59c632b737106072f7891e02253
abcussi <cussi.angel.benjamin@gmail.com>
2020-04-27 12:53:08 -0400

    stickler fix end

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true, length: { maximum: 20, minimum: 3 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, uniqueness: true, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 5,  maximum: 20 }
+  validates :password, presence: true, length: { minimum: 5, maximum: 20 }
   has_secure_password
 end
