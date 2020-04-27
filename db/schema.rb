4fff65fefa9d03f75599b4950dedfb134d1acce8
abcussi <cussi.angel.benjamin@gmail.com>
2020-04-27 10:06:57 -0400

    add coments section validation and conections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_130224) do
+ActiveRecord::Schema.define(version: 2020_04_27_140419) do
+
+  create_table "coments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_coments_on_post_id"
+    t.index ["user_id"], name: "index_coments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -29,5 +39,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_130224) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "coments", "posts"
+  add_foreign_key "coments", "users"
   add_foreign_key "posts", "users"
 end
